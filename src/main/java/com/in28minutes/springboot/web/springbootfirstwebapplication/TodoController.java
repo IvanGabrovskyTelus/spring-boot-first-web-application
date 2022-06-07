3d69fbd5063903664ea28889f3560445555912ef
Ivan Gabrovsky <x255748@corp.ads>
2022-06-07 16:20:23 -0400

    implemented logout functionality

--- src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/TodoController.java
+++ src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/TodoController.java
@@ -19,11 +19,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.model.Todo;
-import com.in28minutes.springboot.web.service.LoginService;
 import com.in28minutes.springboot.web.service.TodoService;
 
 @Controller
-@SessionAttributes("name")
 public class TodoController {
 	
 //	@Autowired
--- src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/WelcomeController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +11,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.in28minutes.springboot.web.service.LoginService;
-
 @Controller
-@SessionAttributes("name")
-public class LoginController {
-	
-//	@Autowired
-	LoginService loginService = new LoginService();
+public class WelcomeController {
 	
 	// show login JSP
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String showLoginPage(ModelMap model) {
-		model.put("name", "in28Minutes");
+	public String showWelcomePage(ModelMap model) {
+		model.put("name", getLoggedinUserName());
 		return "welcome";
+	}
+	
+	private String getLoggedinUserName() {
+		
+		Object principal = SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal();
+		if(principal instanceof UserDetails) {
+			((UserDetails)principal).getUsername();
+		}
+		return principal.toString();
 	}
 	
 }

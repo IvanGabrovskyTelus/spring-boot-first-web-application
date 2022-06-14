031b50f64140f3cbe3d24cf4572e617af6922b9d
Ivan Gabrovsky <x255748@corp.ads>
2022-06-14 16:09:18 -0400

    modified configure function

--- src/main/java/com/in28minutes/springboot/web/security/SecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/web/security/SecurityConfiguration.java
@@ -4,25 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	//Create Users - in28Minutes/dummy
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication()
-        		.withUser("in28Minutes").password("dummy")
-                .roles("USER", "ADMIN");
+		PasswordEncoder encoder = 
+		          PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		    	auth
+		          .inMemoryAuthentication()
+		          .withUser("in28minutes")
+		          .password(encoder.encode("dummy"))
+		          .roles("USER", "ADMIN")
+		          .and()
+		          .withUser("in28Minutes")
+		          .password(encoder.encode("dummy"))
+		          .roles("USER", "ADMIN");
     }
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login").permitAll()
+        http.authorizeRequests().authorizeRequests().anyRequest().antMatchers("/login").permitAll()
                 .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
-                .formLogin();
+                .authenticated().formLogin().httpBasic();
     }
 	
 	//Create a Login form
